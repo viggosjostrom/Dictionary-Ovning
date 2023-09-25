723a3d67f35a278c3a5fe1da7c48191c3a9579fc
viggosjostrom <viggosjostrom1@gmail.com>
2023-09-25 13:08:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+string list = File.ReadAllText("../../../lista.csv");
+
+Dictionary<string, int> husdjur = new Dictionary<string, int>();
+    
+string[] listSplit = list.Split(",");
+
+int age;
+
+foreach (var item in listSplit)
+{
+    Console.WriteLine(item); 
+}
